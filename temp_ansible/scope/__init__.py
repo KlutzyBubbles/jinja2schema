6e04e8b2b1cdef6dbecab80f0d28129873d3f82b
Lee Tzilantonis <lee.tzilantonis@ibm.com>
2021-11-11 13:29:38 +1100

    remove old files

--- temp_ansible/scope/__init__.py
+++
@@ -1,3 +0,0 @@
-from .access_type import AccessType
-from .error_record import ErrorRecord
-from .scope import Scope
--- temp_ansible/test2.py
+++
@@ -1,7 +0,0 @@
-from ansible.module_utils.facts.collector import get_collector_names
-
-# col = BaseFactCollector()
-
-test = get_collector_names()
-
-print(test)
